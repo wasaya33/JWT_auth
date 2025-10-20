df9d8ebb06d11a98fd8656f84043e118a7c882fb
wasaya33 <bsf2201649@ue.edu.pk>
2025-10-20 22:37:34 +0500

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import EmailVerify from './pages/EmailVerify.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
 
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/verify-email' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword  />} />
+      </Routes>
     </div>
   )
 }
---
+++ frontend/src/pages/EmailVerify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmailVerify = () => {
+  return (
+    <div>
+      <h1> Verify Email</h1>
+    </div>
+  )
+}
+
+export default EmailVerify
