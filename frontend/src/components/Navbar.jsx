3745888e7e529086b9ce65a0cedda29fd33bfe3a
wasaya33 <bsf2201649@ue.edu.pk>
2025-10-22 09:03:24 +0500

    Login

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {assets} from './../assets/assets.js'
-
+import  {useNavigate} from 'react-router-dom'
 const Navbar = () => {
+  const navigate=useNavigate();
   return (
     <div className='w-full flex justify-between items-center  p-4  sm:p-6 sm:px-24 absolute top-0'>
       <img src={assets.logo} alt="" className='w-28 sm-32'/>
-       <button className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all '>Login <img src={assets.arrow_icon} alt="" /></button>
+       <button onClick={()=>navigate('/login')}
+        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all '>Login <img src={assets.arrow_icon} alt="" /></button>
     </div>
   )
 }
